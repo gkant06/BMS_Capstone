9ebc42e922b59b17b72f7a7cdb316e5027e140b1
gkant06 <105669929+gkant06@users.noreply.github.com>
2023-04-28 17:51:05 -0400

    Update transfer_learning_clustering.py

--- segmentation/transfer_learning_clustering.py
+++ segmentation/transfer_learning_clustering.py
@@ -1,5 +1,5 @@
 # Clustering using a transfer learning approach. A CNN (VGG16) is used to extract features. This is followed
-# by dimensionality reduction using PCA (linear) and UMAP(non-linear). Then k-means/DBSCAN clustering is done 
+# by dimensionality reduction using PCA (linear) and UMAP(non-linear). Then k-means clustering is done 
 #and clusters are visualized.
 
 
@@ -10,7 +10,6 @@ from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import VGG16 
 from keras.models import Model
 from sklearn.cluster import KMeans
-from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
 import umap
 import umap.plot
@@ -22,8 +21,7 @@ import pandas as pd
 import pickle
 import seaborn as sns
 from sklearn.pipeline import Pipeline
-import os
-
+import pandas as pd
 
 path = '/jet/home/gkant/mAb1_20007#/output_images'
 print(path)
@@ -82,8 +80,8 @@ print(feat.shape)
 pipeline = Pipeline([
     ('pca', PCA(n_components=2, random_state=22)),
     ('umap_mapper', umap.UMAP(n_components=2, random_state=42)),
-    ('kmeans_cluster', KMeans(n_clusters=5,random_state=22)),
-    ('dbscan_cluster', DBSCAN(min_samples = 5))
+    ('kmeans_cluster_pca', KMeans(n_clusters=5,random_state=22)),
+    ('kmeans_cluster_umap', KMeans(n_clusters=2,random_state=22))
 ])
 
 # Fit the pipeline to the feature vectors
@@ -94,12 +92,8 @@ pca_out = pipeline.named_steps['pca'].fit_transform(feat)
 umap_out = pipeline.named_steps['umap_mapper'].fit_transform(feat)
 
 # Get the KMeans cluster labels for PCA and UMAP reduced feature vectors
-pca_kmeans_labels = pipeline.named_steps['kmeans_cluster'].fit_predict(pca_out)
-umap_kmeans_labels = pipeline.named_steps['kmeans_cluster'].fit_predict(umap_out)
-
-# Get the DBSCAN cluster labels for PCA and UMAP reduced feature vectors
-pca_dbscan_labels = pipeline.named_steps['dbscan_cluster'].fit_predict(pca_out)
-umap_dbscan_labels = pipeline.named_steps['dbscan_cluster'].fit_predict(umap_out)
+pca_kmeans_labels = pipeline.named_steps['kmeans_cluster_pca'].fit_predict(pca_out)
+umap_kmeans_labels = pipeline.named_steps['kmeans_cluster_umap'].fit_predict(umap_out)
 
 # create plots directory if it does not exist
 if not os.path.exists('/jet/home/gkant/mAb1_20007#/plots'):
@@ -110,41 +104,96 @@ plt.figure(figsize=(10, 8))
 sns.scatterplot(x=pca_out[:, 0], y=pca_out[:, 1], hue=pca_kmeans_labels)
 plt.savefig('/jet/home/gkant/mAb1_20007#/plots/pca_kmeans.png', dpi=300, bbox_inches='tight')
 
+
 # save UMAP/kmeans plot
 plt.figure(figsize=(10, 8))
 sns.scatterplot(x=umap_out[:, 0], y=umap_out[:, 1], hue=umap_kmeans_labels)
 plt.savefig('/jet/home/gkant/mAb1_20007#/plots/umap_kmeans.png', dpi=300, bbox_inches='tight')
 
-# save PCA/DBSCAN plot
-plt.figure(figsize=(10, 8))
-sns.scatterplot(x=pca_out[:, 0], y=pca_out[:, 1], hue=pca_dbscan_labels)
-plt.savefig('/jet/home/gkant/mAb1_20007#/plots/pca_dbscan.png', dpi=300, bbox_inches='tight')
 
-# save UMAP/DBSCAN plot
-plt.figure(figsize=(10, 8))
-sns.scatterplot(x=umap_out[:, 0], y=umap_out[:, 1], hue=umap_dbscan_labels)
-plt.savefig('/jet/home/gkant/mAb1_20007#/plots/umap_dbscan.png', dpi=300, bbox_inches='tight')
+# holds the cluster id for pca, umap and the images
+groups = {}
+for file, cluster_pca, cluster_umap in zip(filenames, pca_kmeans_labels, umap_kmeans_labels):
+    if cluster_pca not in groups.keys():
+        groups[cluster_pca] = {"pca": [], "umap": []}
+    groups[cluster_pca]["pca"].append(file)
 
-     
+    if cluster_umap not in groups.keys():
+        groups[cluster_umap] = {"pca": [], "umap": []}
+    groups[cluster_umap]["umap"].append(file)
 
-'''
-# function that lets you view a cluster (based on cluster label)        
-def view_cluster(cluster):
-    plt.figure(figsize = (25,25));
+
+# function that lets you view clusters (based on cluster label)        
+def view_cluster(cluster, plot_type="pca", save_dir=None):
+
+    # create subdirectory for cluster if save_dir is specified
+    if save_dir:
+        plot_dir = save_dir
+        if not os.path.exists(plot_dir):
+            os.makedirs(plot_dir)
+    
+    plt.figure(figsize=(20,20))
     # gets the list of filenames for a cluster
-    files = groups[cluster]
+    files = groups.get(cluster, {}).get(plot_type, [])
     # only allow up to 30 images to be shown at a time
     if len(files) > 30:
         print(f"Clipping cluster size from {len(files)} to 30")
         files = files[:29]
-    # plot each image in the cluster
-    for index, file in enumerate(files):
-        plt.subplot(10,10,index+1);
-        img = load_img(file)
+    # create subplot grid
+    num_rows = 6
+    num_cols = 5
+    num_images = min(len(files), num_rows * num_cols)
+    for index in range(num_images):
+        plt.subplot(num_rows, num_cols, index+1)
+        img = load_img(files[index])
         img = np.array(img)
         plt.imshow(img)
         plt.axis('off')
-'''       
+    # save figure if save_dir is specified
+    if save_dir:
+        plot_path = os.path.join(plot_dir, f"cluster_{cluster}.png")
+        plt.savefig(plot_path)
+    plt.show()
+
+# Cluster visualization
+for cluster in set(pca_kmeans_labels):
+    view_cluster(cluster, plot_type="pca", save_dir='/jet/home/gkant/mAb1_20007#/plots/cluster_pca_kmeans/')
+    
+for cluster in set(umap_kmeans_labels):
+    view_cluster(cluster, plot_type="umap", save_dir='/jet/home/gkant/mAb1_20007#/plots/cluster_umap_kmeans/')
+
+
+# Read in the metadata file as a DataFrame
+metadata = pd.read_csv("/jet/home/gkant/mAb1_20007#/metadata.csv")
+
+# Iterate through the file paths in the metadata file
+pca_labels = []
+umap_labels = []
+for filepath in metadata["image"]:
+    # Extract the filename from the filepath
+    filename = os.path.basename(filepath)
+
+    # Look up the corresponding PCA and UMAP cluster labels in the groups dictionary
+    pca_label = None
+    umap_label = None
+    for cluster_label, cluster_data in groups.items():
+        if filename in cluster_data["pca"]:
+            pca_label = cluster_label
+        if filename in cluster_data["umap"]:
+            umap_label = cluster_label
+
+    # Add the PCA and UMAP cluster labels to the lists
+    pca_labels.append(pca_label)
+    umap_labels.append(umap_label)
+
+# Add the PCA and UMAP cluster labels as new columns to the metadata DataFrame
+metadata["pca_cluster_label"] = pca_labels
+metadata["umap_cluster_label"] = umap_labels
+
+# Write the updated metadata file back to disk
+metadata.to_csv("/jet/home/gkant/mAb1_20007#/metadata.csv", index=False)
+
+
 '''
 # To identify no. of clusters using elbow method (pca)
 pca = PCA(n_components = 2)
