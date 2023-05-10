12b80aec8866543eb8bcbc665780cdf36998ae6b
nataliepham6720 <112508461+nataliepham6720@users.noreply.github.com>
2023-05-10 16:08:10 -0400

    Add files via upload

---
+++ feature_extraction/vae_cnn_protein-crystalization.py
@@ -0,0 +1,295 @@
+"""
+VAEs can be used for generative purposes. 
+This code demonstrates VAE using MNIST dataset.
+Just like regular autoencoder VAE returns an array (image) of same domensions
+as input but we can introduce variation by tweaking the latent vector.
+"""
+# https://github.com/bnsreenu/python_for_microscopists/blob/master/178_179_variational_autoencoders_mnist.py
+
+import keras
+from keras.layers import Conv2D, Conv2DTranspose, Input, Flatten, Dense, Lambda, Reshape
+#from keras.layers import BatchNormalization
+from keras.models import Model
+from keras.datasets import mnist
+from keras import backend as K
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load dataset
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+import os
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Define the path to the directory that contains all the image folders
+dir_path = '/jet/home/thanhngp/BMS1/sample_ASO_all' # contains images from "AmSO4_21223", "HELIX_21221", "MIDAS_Plus_21222"
+
+# Define an empty list to store the image data
+data = []
+
+# Use os.listdir() to get a list of all the subdirectories (class labels) in the image folder directory
+# class_labels = os.listdir(dir_path)
+
+# Use a for loop to iterate through all the subdirectories and load the images into the data list
+#for label in class_labels:
+#    sub_dir = os.path.join(dir_path, label)
+#    for file in os.listdir(sub_dir):
+#        image_path = os.path.join(sub_dir, file)
+#        image = cv2.imread(image_path)
+#        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Convert image to RGB
+#        data.append(image)
+print(len(os.listdir(dir_path)))
+
+for file in os.listdir(dir_path):
+    image_path = os.path.join(dir_path, file)
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Convert image to RGB
+    data.append(image)
+
+# Convert the data list to a NumPy array
+data = np.array(data)
+
+# Use train_test_split() to split the data into training and testing sets
+x_train, x_test = train_test_split(data, test_size=0.2, random_state=42)
+
+
+#Normalize and reshape ============
+
+#Norm.
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train / 255
+x_test = x_test / 255
+
+# Reshape 
+img_width  = x_train.shape[1]
+img_height = x_train.shape[2]
+num_channels = 3 #RGB scale --> 3 channel
+x_train = x_train.reshape(x_train.shape[0], img_height, img_width, num_channels)
+x_test = x_test.reshape(x_test.shape[0], img_height, img_width, num_channels)
+input_shape = (img_height, img_width, num_channels)
+
+print(x_train.shape[0], x_test.shape[0])
+print(x_train.shape, x_test.shape)
+print(input_shape)
+# 576 input images of size (512,512,3), 460 train & 116 test
+# ========================
+#View a few images
+plt.figure(1)
+sub = [i for i in range(331,340)]
+for i in range(9):
+    plt.subplot(sub[i])
+    plt.imshow(x_train[42*i][:,:,0])
+
+#plt.subplot(332)
+#plt.imshow(x_train[420][:,:,0])
+plt.savefig('/jet/home/thanhngp/BMS1/sample_data.png')
+plt.show()
+
+
+# BUILD THE MODEL
+
+# # ================= #############
+# # Encoder
+#Let us define 4 conv2D, flatten and then dense
+# # ================= ############
+latent_dim = 2 # Number of latent dim parameters
+
+input_img = Input(shape=input_shape, name='encoder_input')
+x = Conv2D(32, 3, padding='same', activation='relu')(input_img)
+x = Conv2D(64, 3, padding='same', activation='relu',strides=(2, 2))(x)
+x = Conv2D(64, 3, padding='same', activation='relu')(x)
+x = Conv2D(64, 3, padding='same', activation='relu')(x)
+
+conv_shape = K.int_shape(x) #Shape of conv to be provided to decoder
+#Flatten
+x = Flatten()(x)
+x = Dense(64, activation='relu')(x)
+
+# Two outputs, for latent mean and log variance (std. dev.)
+#Use these to sample random variables in latent space to which inputs are mapped. 
+z_mu = Dense(latent_dim, name='latent_mu')(x)   #Mean values of encoded input
+z_sigma = Dense(latent_dim, name='latent_sigma')(x)  #Std dev. (variance) of encoded input
+
+#REPARAMETERIZATION TRICK
+# Define sampling function to sample from the distribution
+# Reparameterize sample based on the process defined by Gunderson and Huang
+# into the shape of: mu + sigma squared x eps
+#This is to allow gradient descent to allow for gradient estimation accurately. 
+def sample_z(args):
+  z_mu, z_sigma = args
+  eps = K.random_normal(shape=(K.shape(z_mu)[0], K.int_shape(z_mu)[1]))
+  return z_mu + K.exp(z_sigma / 2) * eps
+
+# sample vector from the latent distribution
+# z is the labda custom layer we are adding for gradient descent calculations
+  # using mu and variance (sigma)
+z = Lambda(sample_z, output_shape=(latent_dim, ), name='z')([z_mu, z_sigma])
+
+#Z (lambda layer) will be the last layer in the encoder.
+# Define and summarize encoder model.
+encoder = Model(input_img, [z_mu, z_sigma, z], name='encoder')
+print(encoder.summary())
+
+# ================= ###########
+# Decoder
+#
+# ================= #################
+
+# decoder takes the latent vector as input
+decoder_input = Input(shape=(latent_dim, ), name='decoder_input')
+
+# Need to start with a shape that can be remapped to original image shape as
+#we want our final utput to be same shape original input.
+#So, add dense layer with dimensions that can be reshaped to desired output shape
+x = Dense(conv_shape[1]*conv_shape[2]*conv_shape[3], activation='relu')(decoder_input)
+# reshape to the shape of last conv. layer in the encoder, so we can 
+x = Reshape((conv_shape[1], conv_shape[2], conv_shape[3]))(x)
+# upscale (conv2D transpose) back to original shape
+# use Conv2DTranspose to reverse the conv layers defined in the encoder
+x = Conv2DTranspose(32, 3, padding='same', activation='relu',strides=(2, 2))(x)
+#Can add more conv2DTranspose layers, if desired. 
+#Using sigmoid activation
+x = Conv2DTranspose(num_channels, 3, padding='same', activation='sigmoid', name='decoder_output')(x)
+
+# Define and summarize decoder model
+decoder = Model(decoder_input, x, name='decoder')
+decoder.summary()
+
+# apply the decoder to the latent sample 
+z_decoded = decoder(z)
+
+
+# =========================
+#Define custom loss
+#VAE is trained using two loss functions reconstruction loss and KL divergence
+#Let us add a class to define a custom layer with loss
+class KL_loss(keras.layers.Layer):
+
+    def vae_loss(self, x, z_decoded):
+        x = K.flatten(x)
+        z_decoded = K.flatten(z_decoded)
+        
+        # Reconstruction loss (as we used sigmoid activation we can use binarycrossentropy)
+        recon_loss = keras.metrics.binary_crossentropy(x, z_decoded)
+        
+        # KL divergence
+        kl_loss = -5e-4 * K.mean(1 + z_sigma - K.square(z_mu) - K.exp(z_sigma), axis=-1)
+        return K.mean(recon_loss + kl_loss)
+
+    # add custom loss to the class
+    def call(self, inputs):
+        x = inputs[0]
+        z_decoded = inputs[1]
+        loss = self.vae_loss(x, z_decoded)
+        self.add_loss(loss, inputs=inputs)
+        return x
+
+# apply the custom loss to the input images and the decoded latent distribution sample
+out = KL_loss()([input_img, z_decoded])
+# out is basically the original image after encoding input img to mu, sigma, z
+# and decoding sampled z values.
+#This will be used as output for vae
+
+# =================
+# VAE 
+# =================
+vae = Model(input_img, out, name='vae')
+
+# Compile VAE
+vae.compile(optimizer='adam', loss=None)
+vae.summary()
+
+# Train autoencoder
+print('training ...')
+vae.fit(x_train, None, epochs = 10, batch_size = 32, validation_split = 0.1)
+
+# =================
+# Visualize results
+# =================
+#Visualize inputs mapped to the Latent space
+#Remember that we have encoded inputs to latent space dimension = 2. 
+#Extract z_mu --> first parameter in the result of encoder prediction representing mean
+
+mu, sigma, z = encoder.predict(x_test)
+print('z_mu: ', mu.shape)
+print('sigma: ', sigma.shape)
+print('z: ', z.shape)
+
+# K-means clustering - baseline result
+from sklearn.cluster import KMeans
+# data = np.array(train_data)
+Z = np.hstack(( mu,sigma,z ))
+print(Z.shape)
+n = 10
+kmeans = KMeans(init="k-means++", n_clusters=n)
+model = kmeans.fit(Z)
+labels = model.predict(Z)
+print(labels.shape)
+
+import pandas as pd
+# test_label = np.hstack((x_test, Z, labels))
+df = pd.DataFrame({'x_test': list(x_test), 'Z': list(Z), 'label': list(labels)})
+df.to_csv('/jet/home/thanhngp/BMS1/combine_test_label.csv')
+
+
+from sklearn.manifold import TSNE
+
+tsne = TSNE(n_components=2)
+mu_tsne = tsne.fit_transform(Z)
+
+#Plot dim1 and dim2 for mu
+plt.figure(figsize=(10, 10))
+# plt.scatter(mu[:, 0], mu[:, 1], c=labels, cmap='brg')
+plt.scatter(mu_tsne[:, 0], mu_tsne[:, 1], c=labels, cmap='brg')
+plt.xlabel('dim 1')
+plt.ylabel('dim 2')
+plt.colorbar()
+plt.savefig('/jet/home/thanhngp/BMS1/latent_space_with_tsne.png')
+plt.show()
+
+
+# Visualize images
+#Single decoded image with random input latent vector (of size 1x2)
+#Latent space range is about -5 to 5 so pick random values within this range
+#Try starting with -1, 1 and slowly go up to -1.5,1.5 and see how it morphs from 
+#one image to the other.
+sample_vector = np.array([[1,-1]])
+decoded_example = decoder.predict(sample_vector)
+decoded_example_reshaped = decoded_example.reshape(img_width, img_height, 3)
+plt.imshow(decoded_example_reshaped)
+
+#Let us automate this process by generating multiple images and plotting
+#Use decoder to generate images by tweaking latent variables from the latent space
+#Create a grid of defined size with zeros. 
+#Take sample from some defined linear space. In this example range [-1, 1]
+#Feed it to the decoder and update zeros in the figure with output.
+
+
+n = 10 # generate 15x15 digits
+figure = np.zeros((img_width * n, img_height * n, num_channels))
+
+#Create a Grid of latent variables, to be provided as inputs to decoder.predict
+#Creating vectors within range -5 to 5 as that seems to be the range in latent space
+grid_x = np.linspace(-1,1, n)
+grid_y = np.linspace(-1,1, n)[::-1]
+
+# decoder for each square in the grid
+for i, yi in enumerate(grid_y):
+    for j, xi in enumerate(grid_x):
+        z_sample = np.array([[xi, yi]])
+        x_decoded = decoder.predict(z_sample)
+        digit = x_decoded[0].reshape(img_width, img_height, num_channels)
+        figure[i * img_width: (i + 1) * img_width,
+               j * img_height: (j + 1) * img_height] = digit
+
+plt.figure(figsize=(10, 10))
+#Reshape for visualization
+fig_shape = np.shape(figure)
+figure = figure.reshape((fig_shape[0], fig_shape[1], 3))
+
+plt.imshow(figure, cmap='gnuplot2')
+plt.savefig('/jet/home/thanhngp/BMS1/manifold.png')
+plt.show()  
